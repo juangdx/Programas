040d561772a4513056c0d9d605d3ba004ff504b4
juangdx <vicovic@gmail.com>
2026-02-18 20:53:38 +0100

    Delete TEST.h

--- TEST.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-#include <stdio.h>
-
-#define INICIO_TEST() printf("Inicio pruebas\n")
-#define PRINT_TEST(formato, valor) printf("---PASA: Salida Correcta para entrada: " formato "\n", valor)
-#define FIN_TEST() printf("Se pasan todas las pruebas.\n")
-
-#endif
