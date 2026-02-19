5909e5ff1f083bbf1f87586458a93f039d0c3cb9
juangdx <vicovic@gmail.com>
2026-02-19 19:13:38 +0100

    Create TEST.h

---
+++ .vscode/test/TEST.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+#include <stdio.h>
+
+// Estas macros son las que usa tu código
+#define INICIO_TEST() printf("Inicio pruebas\n")
+#define FIN_TEST()    printf("Fin pruebas\n")
+#define PRINT_TEST(format, var) printf(format, var)
+
+#endif
